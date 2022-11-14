dcaddc65e7930ebd6ff6e9f629d314dee29c024d
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-11-14 20:24:04 +0100

    feat: Custom configuration updates

--- node/.eslintrc.js
+++ node/.eslintrc.js
@@ -4,75 +4,87 @@ module.exports = {
     node: true,
   },
 
-  extends: ['airbnb-base', 'plugin:import/typescript', 'prettier'],
+  extends: ["airbnb-base", "plugin:import/typescript", "prettier"],
 
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
 
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
 
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
 
-    'import/resolver': {
+    "import/resolver": {
       typescript: {
         alwaysTryTypes: true,
       },
     },
   },
 
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ["@typescript-eslint", "jest"],
 
   rules: {
-    'no-console': 0,
-    'no-use-before-define': 0,
-    'comma-dangle': 0,
+    "no-console": 0,
+    "no-use-before-define": 0,
+    "comma-dangle": 0,
 
-    'wrap-iife': ['error', 'inside'],
+    "wrap-iife": ["error", "inside"],
 
-    'import/extensions': [
-      'error',
-      'ignorePackages',
+    "import/extensions": [
+      "error",
+      "ignorePackages",
       {
-        ts: 'never',
-        tsx: 'never',
-        mts: 'never',
-        cts: 'never',
+        ts: "never",
+        tsx: "never",
+        mts: "never",
+        cts: "never",
       },
     ],
 
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-
-    'import/prefer-default-export': 0,
-
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': 'error',
-
-    'no-underscore-dangle': 0,
-    'no-useless-escape': 0,
-
-    'import/order': 0,
-    'import/no-named-default': 0,
-
-    'no-unused-expressions': ['error', { allowShortCircuit: true }],
-
-    'no-redeclare': 'off',
-    '@typescript-eslint/no-redeclare': ['error'],
-
-    'lines-between-class-members': 'off',
-    'class-methods-use-this': 'off',
-
-    'consistent-return': 'warn',
-    'no-param-reassign': ['error', { props: false }],
-
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['test/**/*.ts', 'tests/**/*.ts', 'src/lib/**/test.ts', 'jest.config.ts'] },
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
     ],
+
+    "import/prefer-default-export": 0,
+
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "error",
+
+    "no-underscore-dangle": 0,
+    "no-useless-escape": 0,
+
+    "import/order": 0,
+    "import/no-named-default": 0,
+
+    "no-unused-expressions": ["error", { allowShortCircuit: true }],
+
+    "no-redeclare": "off",
+    "@typescript-eslint/no-redeclare": ["error"],
+
+    "lines-between-class-members": "off",
+    "class-methods-use-this": "off",
+
+    "consistent-return": "warn",
+    "no-param-reassign": ["error", { props: false }],
+
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "test/**/*.ts",
+          "tests/**/*.ts",
+          "src/lib/**/test.ts",
+          "jest.config.ts",
+        ],
+      },
+    ],
+
+    "no-return-await": "off",
   },
 };
--- node/jest.config.ts
+++ node/jest.config.ts
@@ -1,53 +1,44 @@
-import type { Config } from '@jest/types';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defaults } from 'jest-config';
+import { defaults } from "jest-config";
+import type { JestConfigWithTsJest } from "ts-jest";
+// import type { Config } from '@jest/types';
 
 const areWeTestingLibs = process.env?.FOR_LIB ?? false;
 const isCI = process.env?.CI ?? false;
 
 let collectCoverageFrom = areWeTestingLibs
-  ? ['src/lib/**']
-  : ['src/*/{*.ts,!(lib)/**/*.ts}'];
+  ? ["src/lib/**"]
+  : ["src/*/{*.ts,!(lib)/**/*.ts}"];
 
-let testPathIgnorePatterns = areWeTestingLibs ? ['/tests/'] : ['src/lib/'];
+let testPathIgnorePatterns = areWeTestingLibs ? ["/tests/"] : ["src/lib/"];
 
 if (isCI) {
   collectCoverageFrom = [];
   testPathIgnorePatterns = defaults.testPathIgnorePatterns;
 }
 
-testPathIgnorePatterns = testPathIgnorePatterns.concat(['/tests/test-data/']);
+testPathIgnorePatterns = testPathIgnorePatterns.concat(["/tests/test-data/"]);
 
-const config: Config.InitialOptions = {
-  preset: 'ts-jest/presets/default-esm',
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/default-esm",
   testTimeout: 10000,
-  testEnvironment: 'node',
+  testEnvironment: "node",
   verbose: true,
-  notify: true,
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  notify: !isCI,
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
   collectCoverageFrom,
   testPathIgnorePatterns,
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  transform: {},
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
+  setupFilesAfterEnv: ["jest-extended/all", "<rootDir>/tests/setup.ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
-  moduleFileExtensions: ['js', 'ts', 'mjs'],
-  moduleNameMapper: {
-    '#ansi-styles':
-      '<rootDir>/node_modules/zx/node_modules/chalk/source/vendor/ansi-styles/index.js',
-    '#supports-color':
-      '<rootDir>/node_modules/zx/node_modules/chalk/source/vendor/supports-color/index.js',
-    '@app/(.*)': '<rootDir>/src/app/$1',
-    '@cmds/(.*)': '<rootDir>/src/cmds/$1',
-    '@lib/(.*)': '<rootDir>/src/lib/$1',
-    '@config/(.*)': '<rootDir>/src/config/$1',
-    '@utils': '<rootDir>/src/utils/index',
-    '@types': '<rootDir>/src/types/index',
-  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleFileExtensions: ["js", "ts", "mjs"],
   watchman: false,
 };
 
